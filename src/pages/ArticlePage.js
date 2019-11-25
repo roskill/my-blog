f78f80e82983927e4577330cd41aa12117823767
Justin Liu <justin.liu@myob.com>
2019-11-25 14:38:08 +1100

    Complete frontend

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import articleContent from './article-content';
+import NotFoundPage from './NotFoundPage';
+
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
+
+  if (!article) return <NotFoundPage />;
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
+};
+
+export default ArticlePage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <h1>Welcome to my blog</h1>
+    <p>
+      Lorem ipsum dolor amet authentic crucifix copper mug, la croix VHS hella
+      ethical activated charcoal chillwave kogi banh mi. +1 hell of flannel
+      venmo, vape cray hella truffaut kickstarter ennui artisan cliche blue
+      bottle dreamcatcher. Tbh tilde PBR&B heirloom kinfolk brunch. Poke
+      pour-over chicharrones etsy sartorial echo park next level woke vaporware
+      gluten-free. Fam helvetica chambray man braid biodiesel pabst. Sartorial
+      photo booth put a bird on it pickled gochujang coloring book cred freegan
+      artisan kickstarter. Blog lyft before they sold out, scenester snackwave
+      trust fund fashion axe enamel pin vexillologist lomo literally.
+    </p>
+    <p>
+      Chia tbh leggings bitters air plant. Shoreditch brunch irony, mumblecore
+      aesthetic hashtag artisan four dollar toast put a bird on it PBR&B next
+      level bespoke kinfolk authentic. Shabby chic VHS hell of gluten-free
+      sustainable pickled food truck disrupt thundercats. Scenester enamel pin
+      listicle, art party raw denim forage tbh fanny pack. Small batch austin
+      wayfarers cred listicle.
+    </p>
+    <p>
+      Hoodie kinfolk palo santo listicle skateboard ennui hammock live-edge
+      umami deep v poke next level. Meditation vegan you probably haven't heard
+      of them schlitz sriracha blog DIY iPhone. Drinking vinegar vaporware
+      pop-up whatever tofu readymade fashion axe wolf. Salvia biodiesel palo
+      santo snackwave next level paleo VHS.
+    </p>
+    <p>
+      Four dollar toast adaptogen normcore tilde humblebrag vape man bun. Copper
+      mug air plant kickstarter master cleanse waistcoat fingerstache.
+      Fingerstache street art williamsburg, hoodie blue bottle activated
+      charcoal offal lomo keytar bicycle rights. Shoreditch cray hammock man
+      braid cloud bread portland. Godard waistcoat poke, prism butcher crucifix
+      pop-up retro blog art party.
+    </p>
+    <p>
+      Hot chicken kombucha single-origin coffee seitan letterpress. Mumblecore
+      locavore hella, shoreditch vexillologist meggings tumeric pok pok taiyaki
+      swag selfies DIY irony. DIY snackwave direct trade everyday carry you
+      probably haven't heard of them glossier 90's shoreditch pabst readymade.
+      Seitan chillwave pitchfork succulents palo santo keytar kombucha.
+    </p>
+  </>
+);
+
+export default HomePage;
