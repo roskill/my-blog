211d9da1c36848e7cd51323bffff8be73a5f9218
Justin Liu <justin.liu@myob.com>
2019-11-26 20:28:49 +1100

    Add comment form

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -22,11 +24,16 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This post has been upvoted {articleInfo.upvotes} times</p>
+      <UpvotesSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
       <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
     </>
   );
 };
